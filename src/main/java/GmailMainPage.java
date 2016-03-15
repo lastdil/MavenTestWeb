ee1b14ea500e5ae637e2905665f0febcf7f7e436
lastdil <idrygin@gmail.com>
2016-03-15 15:38:41 +0300

    1

---
+++ src/main/java/GmailMainPage.java
@@ -0,0 +1,78 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+
+public class GmailMainPage {
+    private WebDriver driver;
+    //Locators
+
+    @FindBy(xpath = ("//div[@class='z0']/div"))
+    private WebElement ComposeMail;
+
+
+    @FindBy(className = "vO")
+    private WebElement Recepient;
+
+    @FindBy(className = "aoT")
+    private WebElement Subject;
+
+    @FindBy(xpath = ("//div[@aria-label='Send ‪(Ctrl-Enter)‬']"))
+    private WebElement SendMail;
+
+    @FindBy(css = ".asf.T-I-J3.J-J5-Ji")
+    private WebElement Refresh;
+
+    @FindBy(id = "gs_taif50")
+    private WebElement SearchOnMail;
+
+    @FindBy(xpath = "//div[contains(@id,':32')]//")
+    private WebElement SelectArrow;
+
+    @FindBy(xpath = "//div[contains(.,'Unread')]")
+    private WebElement Unread;
+
+    @FindBy(xpath = ".//*[@id=':l6']/div")
+    private WebElement UnreadMess;
+
+    @FindBy(xpath = ".//*[@id=':35']/div")
+    private WebElement More;
+
+    @FindBy(xpath = ".//*[@id=':mm']/div")
+    private WebElement MarkAsRead;
+    //Constructor
+
+    public GmailMainPage(WebDriver driver) {
+        this.driver = driver;
+
+        // Init
+
+        PageFactory.initElements(driver, this);
+    }
+        public void CreateNewMail(){
+
+            ComposeMail.click();
+        }
+        public void EnterRecepient(String recepient){
+
+            Recepient.sendKeys(recepient);
+        }
+        public void EnterSubject(String subject){
+        Subject.sendKeys(subject);
+    }
+        public void SendMail(){
+            SendMail.click();
+        }
+
+    public void Refresh(){
+        Refresh.click();
+    }
+    public void Select(){
+        SelectArrow.click();
+    }
+    public void Unread(){
+        Unread.click();
+    }
+}
--- src/test/java/GmailLogPageTest.java
+++ src/test/java/GmailLogPageTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Wait;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -15,25 +16,37 @@ public class GmailLogPageTest {
     WebDriver driver;
 
     @BeforeTest
-    public void setup(){
+    public void setup() throws Exception {
         System.setProperty("webdriver.chrome.driver", "browsers/chromedriver.exe");
         driver = new ChromeDriver();
-
+        GmailLogPage page = new GmailLogPage(driver);
+        driver.get("https://mail.google.com");
+        page.SetEmail("your_email");
+        page.Next();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        page.SetUserPwd("Your_PWD");
+        page.SetCheckBox();
+        page.SingIn();
+        if (page.cheker.isDisplayed()) {
+            System.out.println("Login Success");
+        }
+        Thread.sleep(1000);
     }
-        @Test
-    public void SingIn()throws Exception{
-            GmailLogPage page = new GmailLogPage(driver);
-            driver.get("https://mail.google.com");
-            page.SetEmail("idrygin@gmail.com");
-            page.Next();
-            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-            page.SetUserPwd("you_pwd");
-            page.SetCheckBox();
-            page.SingIn();
-            if (page.cheker.isDisplayed()){
-                System.out.println("OK");
-            }
 
+        @Test
+        public void ComposeMail() throws Exception{
+            GmailMainPage page1 = new GmailMainPage(driver);
+            page1.CreateNewMail();
+            //enter details
+            page1.EnterRecepient("idrygin@gmail.com");
+            page1.EnterSubject("its works!");
+            page1.SendMail();
+            page1.Refresh();
+        }
+
+        @AfterTest
+            public void TearDown() throws Exception {
+            driver.quit();
         }
 
 }
